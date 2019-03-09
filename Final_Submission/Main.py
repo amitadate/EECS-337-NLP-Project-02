f6849a74b22b3c0955065f4295fb8a5744898199
omkarstpt <omkarstpt@gmail.com>
2019-03-09 16:21:40 -0600

    Integrated mexican, indian and italian

--- Final_Submission/Main.py
+++ Final_Submission/Main.py
@@ -1,7 +1,7 @@
 from url_srapper import getScrapedData
 from data_extractor import methods_tools_extracter, ingredients_extracter, nutrition_extracter
 from transformer import replace_instructions
-from transformation_list import healthy, vegetarian, to_vegan_list, normal_to_chinese
+from transformation_list import healthy, vegetarian, to_vegan_list, normal_to_chinese, x_to_mexican, x_to_indian, x_to_italian
 from extracter_list import PRIMARY_COOKING_METHODS, SECONDARY_COOKING_METHODS, TOOLS, UNITS, DESCRIPTOR, all_food
 
 food_link1 = "https://www.allrecipes.com/recipe/222000/spaghetti-aglio-e-olio"
@@ -25,7 +25,7 @@ methods = methods_tools_extracter(scrapped_data['directions'], PRIMARY_COOKING_M
 print('', end='')
 for each in methods:
     print("\n * ", each)
-    for e in methods[each]: 
+    for e in methods[each]:
         print("   ", e, " - ", ", ".join(methods[each][e]))
 print("\n\n")
 
@@ -51,4 +51,22 @@ print("****************************************** Transformed to Chinese *******
 transformed_chinese_method = replace_instructions(methods, normal_to_chinese)
 print(' * ', end='')
 print("\n * ".join(transformed_chinese_method))
+print("\n\n")
+
+print("****************************************** Transformed to Mexican ****************************************************")
+transformed_mexican_method = replace_instructions(methods, x_to_mexican)
+print(' * ', end='')
+print("\n * ".join(transformed_mexican_method))
+print("\n\n")
+
+print("****************************************** Transformed to Indian ****************************************************")
+transformed_indian_method = replace_instructions(methods, x_to_indian)
+print(' * ', end='')
+print("\n * ".join(transformed_indian_method))
+print("\n\n")
+
+print("****************************************** Transformed to Italian ****************************************************")
+transformed_italian_method = replace_instructions(methods, x_to_italian)
+print(' * ', end='')
+print("\n * ".join(transformed_italian_method))
 print("\n\n")
--- Final_Submission/transformation_list.py
+++ Final_Submission/transformation_list.py
@@ -261,3 +261,63 @@ normal_to_chinese = {
     "meat": "Bakkwa",
     "lamb": "lamb broth",
 }
+
+
+x_to_mexican = {
+    'beef': 'carnitas',
+    'steak': 'carnitas',
+    'fish': 'chicken',
+    'bread': 'tortilla',
+    'potatoes': 'refried beans',
+    'basil': 'cilantro',
+    'parsley': 'cilantro',
+    'bell pepper': 'chili pepper',
+    'string bean': 'kidney bean',
+    'peas': 'black beans',
+    'rice': 'quinoa',
+    'turnip': 'corn',
+    'carrot': 'corn',
+    'eggplant': 'tomato',
+    'leek': 'onion',
+    'garlic': 'onion',
+    'parsley': 'cilantro',
+    'basil': 'cilantro',
+    'flour tortillas': 'corn tortillas',
+    'mushrooms': 'green peppers',
+    'parmesan cheese': 'queso fresco',
+    'lemon': 'lime',
+    'lemon juice': 'lice juice',
+    'coriander': 'paprika',
+    'turmeric': 'paprika',
+    'teriyaki sauce': 'black mole sauce',
+    'ginger': 'habanero',
+    'ginger root': 'habanero',
+    'olives': 'jalapenos'
+}
+
+
+x_to_indian = {
+    'beef': 'chicken',
+    'steak': 'chicken',
+    'pork': 'lamb',
+    'bread': 'naan',
+    'sandwich': 'flatbread',
+    'noodles': 'rice',
+    'pasta': 'rice'
+}
+
+x_to_italian = {
+    'rice': 'pasta',
+    'noodles': 'pasta',
+    'potatoes': 'noodles',
+    'potato': 'noodles',
+    'corn': 'bell pepper',
+    'turnip': 'onion',
+    'carrot': 'onion',
+    'bok choy': 'lettuce',
+    'cilantro': 'basil',
+    'chives': 'parsley',
+    'jalapeno': 'bell pepper',
+    'cayenne': 'bell pepper',
+    'ginger': 'garlic'
+}
