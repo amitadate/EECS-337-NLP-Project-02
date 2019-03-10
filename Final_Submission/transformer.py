ed19d4268001410cf9ef3e94cb69781823ab5ad2
omkarstpt <omkarstpt@gmail.com>
2019-03-09 19:19:04 -0600

    Changed transformer

--- Final_Submission/transformer.py
+++ Final_Submission/transformer.py
@@ -1,12 +1,30 @@
-def replace_instructions(steps, replace):
+def replace_instructions_v1(steps, replace):
     update_dir = []
     for each in steps:
         for e_replace in replace:
             if e_replace in each:
                 each = each.replace(e_replace, replace[e_replace])
-                break
             elif e_replace in each.lower():
                 each = each.lower().replace(e_replace, replace[e_replace])
-                break
         update_dir.append(each.strip())
+    return update_dir
+
+
+def replace_instructions(steps, replace):
+    update_dir = []
+    for step in steps:
+        step_list = step.split(" ")
+        for ing in replace:
+            for i in range(1, len(step_list)):
+                if (step_list[i - 1].lower() + " " + step_list[i].lower()) == ing:
+                    step_list[i - 1] = replace[ing]
+                    step_list[i] = ''
+                elif step_list[i - 1].lower() == ing:
+                    step_list[i - 1] = replace[ing]
+                if i == len(step_list) - 1:
+                    if step_list[i].lower() == ing:
+                        step_list[i] = replace[ing]
+
+        step_remade = " ".join(step_list)
+        update_dir.append(step_remade)
     return update_dir
