98add47b3ea8c6b02dbfc149e8d5e12118164a14
omkarstpt <omkarstpt@gmail.com>
2019-03-09 18:01:26 -0600

    Breaking after each replacement

--- Final_Submission/transformer.py
+++ Final_Submission/transformer.py
@@ -4,7 +4,9 @@ def replace_instructions(steps, replace):
         for e_replace in replace:
             if e_replace in each:
                 each = each.replace(e_replace, replace[e_replace])
+                break
             elif e_replace in each.lower():
                 each = each.lower().replace(e_replace, replace[e_replace])
+                break
         update_dir.append(each.strip())
     return update_dir
